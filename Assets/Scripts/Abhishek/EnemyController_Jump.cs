50f4b35bf34bf429f916e0f05c6761e57473a727
Андрей <65764445+Neagz@users.noreply.github.com>
2024-08-27 18:39:43 +0300

    Commit

---
+++ Assets/Scripts/Abhishek/EnemyController_Jump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController_Jump : MonoBehaviour
+{
+    public float JumpSpeed = 10f;
+    public bool isGround;
+    private Rigidbody2D rigidBody;
+    
+    void Start()
+    {
+        isGround = true;
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if(isGround == true)
+        {
+            rigidBody.velocity = new Vector2(0, JumpSpeed);
+            isGround = false;
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D other) 
+    {
+        if (other.tag == "GroundCheck")
+        {
+            isGround = true;
+        }
+    }
+}
